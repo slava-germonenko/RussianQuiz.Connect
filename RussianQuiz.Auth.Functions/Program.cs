6dd5d342f3ef2e3747bd9f54161e36ce2f82f316
Slava Germonenko <slava.germonenko@gmail.com>
2021-09-10 19:26:35 +0300

    Configured app so it can pull configuration from 3 different sources.
    Now it's possible to use local settings, azure app configuration and azure key vault as configuration providers.

--- RussianQuiz.Auth.Functions/Program.cs
+++ RussianQuiz.Auth.Functions/Program.cs
@@ -1,3 +1,10 @@
+using System;
+
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 
@@ -9,9 +16,44 @@ namespace RussianQuiz.Auth.Functions
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services =>
+                {
+                    var configuration = BuildConfiguration();
+                    services.AddSingleton(configuration);
+                })
                 .Build();
 
             host.Run();
+        }
+
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+            string localSettingsFileName = Environment.GetEnvironmentVariable("LocalSettingsFile");
+            if (!string.IsNullOrEmpty(localSettingsFileName))
+            {
+                configurationBuilder.AddJsonFile(localSettingsFileName);
+            }
+
+            string appConfigurationConnectionString = Environment.GetEnvironmentVariable("AppConfigurationConnectionString");
+            if (!string.IsNullOrEmpty(appConfigurationConnectionString))
+            {
+                configurationBuilder.AddAzureAppConfiguration(appConfigurationConnectionString);
+            }
+
+            string keyVaultUrl = Environment.GetEnvironmentVariable("KeyVaultUrl");
+            if (!string.IsNullOrEmpty(keyVaultUrl))
+            {
+                var secretClient = new SecretClient(
+                    new Uri(keyVaultUrl),
+                    new DefaultAzureCredential()
+                );
+
+                configurationBuilder.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+            }
+
+            return configurationBuilder.Build();
         }
     }
 }
